62106a2ca8fb936814e6547088134481b4a81774
KENNEDY COSTA <kennedy.costa.valenet@gmail.com>
2022-10-25 19:02:07 -0300

    useState, useEffect, useParams

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,10 +1,31 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Product } from 'types/products';
+import { BASE_URL } from 'util/requests';
 
 import './styles.css';
 
+type UrlParams = {
+  productId: string;
+}
+
+
 const ProductDetails = () => {
+
+  const { productId } = useParams<UrlParams>();
+  
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/products/${productId}`)
+    .then( response => {
+      setProduct(response.data);
+    });
+  }, [productId]);
+
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
@@ -18,25 +39,20 @@ const ProductDetails = () => {
           <div className="col-xl-6">
             <div className="img-container">
               <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
-                alt="Nome do produto"
+                src={product?.imgUrl}
+                alt={product?.name}
               />
             </div>
             <div className="name-price-container">
-              <h1>Nome do Produto</h1>
-              <ProductPrice price={2345.67} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price}/>}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do produto</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Deleniti saepe dolor, et quo facere magnam nobis, odio iste
-                ullam explicabo, velit ex minus rem? Molestiae quas fugit
-                asperiores commodi magni corporis quis dolor aspernatur,
-                accusantium aperiam assumenda rem minima, sapiente totam
-                inventore pariatur id alias.
+                {product?.description}
               </p>
             </div>
           </div>
