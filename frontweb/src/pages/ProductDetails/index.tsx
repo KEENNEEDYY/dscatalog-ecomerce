7e6eaf89188512e149e2273df84442a0e82bebe1
KENNEDY COSTA <kennedy.costa.valenet@gmail.com>
2022-11-03 12:04:12 -0300

    ProductDetails loaders

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Product } from 'types/products';
 import { BASE_URL } from 'util/requests';
+import ProductDetailsLoader from './ProductDetailsLoader';
+import ProductInfoLoader from './ProductInfoLoader';
 
 import './styles.css';
 
@@ -19,10 +21,16 @@ const ProductDetails = () => {
   
   const [product, setProduct] = useState<Product>();
 
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
+    setIsLoading(true);
     axios.get(`${BASE_URL}/products/${productId}`)
     .then( response => {
       setProduct(response.data);
+    })
+    .finally(() => {
+      setIsLoading(false);
     });
   }, [productId]);
 
@@ -37,6 +45,8 @@ const ProductDetails = () => {
         </Link>
         <div className="row">
           <div className="col-xl-6">
+            {isLoading ? <ProductInfoLoader /> :
+            <>
             <div className="img-container">
               <img
                 src={product?.imgUrl}
@@ -47,14 +57,18 @@ const ProductDetails = () => {
               <h1>{product?.name}</h1>
               {product && <ProductPrice price={product?.price}/>}
             </div>
+            </>
+            }
           </div>
           <div className="col-xl-6">
+            {isLoading ? <ProductDetailsLoader /> :
             <div className="description-container">
               <h2>Descrição do produto</h2>
               <p>
                 {product?.description}
               </p>
             </div>
+            }
           </div>
         </div>
       </div>
